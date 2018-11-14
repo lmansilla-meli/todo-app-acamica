2f2f932e4a217444b0c825368fde6df09143e4ab
joelLavaisse <joel.lavaisse@gmail.com>
2018-11-13 21:27:09 -0300

    Se agrega verificacion de credenciales user y admin.

--- index.js
+++ index.js
@@ -4,31 +4,47 @@ var express = require('express');
 var app = express();
 app.use(bodyParser());
 
+var tieneCredenciales = function(req, user, pass){
+    return req.query.user === user && req.query.pass === pass;
+}
+
 app.get('/todos', function(req, res) {
-    fs.readFile('./tareas.json', function(e, data){
-        res.send(JSON.parse(data));
-    });
+    if(tieneCredenciales(req, 'admin', 'admin') || tieneCredenciales(req, 'cliente', '1234')){
+        fs.readFile('./tareas.json', function(e, data){
+            res.send(JSON.parse(data));
+        });
+    } else {
+        res.send('No tiene credenciales.');
+    }; 
 });
 
 app.get('/todos/:todoId', function(req, res) {
-    fs.readFile('./tareas.json', function(e, data){
-        var tasks = JSON.parse(data);
-        res.send(tasks[req.params.todoId]);
-    });
+    if(tieneCredenciales(req, 'admin', 'admin') || tieneCredenciales(req, 'cliente', '1234')){
+        fs.readFile('./tareas.json', function(e, data){
+            var tasks = JSON.parse(data);
+            res.send(tasks[req.params.todoId]);
+        });
+    } else {
+        res.send('No tiene credenciales.');
+    };
 });
 
 app.post('/todos/:todoId', function(req, res) {
-    fs.readFile('./tareas.json', function(e, data){
-        var tasks = JSON.parse(data);
-        tasks[req.params.todoId] = req.body;
-        fs.writeFile('./tareas.json', JSON.stringify(tasks), function() {
-            res.send(req.params.todoId + ' guardado correctamente');
+    if(tieneCredenciales(req, 'admin', 'admin') || tieneCredenciales(req, 'cliente', '1234')){
+        fs.readFile('./tareas.json', function(e, data){
+            var tasks = JSON.parse(data);
+            tasks[req.params.todoId] = req.body;
+            fs.writeFile('./tareas.json', JSON.stringify(tasks), function() {
+                res.send(req.params.todoId + ' guardado correctamente');
+            });
         });
-    });
+    } else {
+        res.send('No tiene credenciales.');
+    };
 });
 
 app.delete('/todos/:todoId', function(req, res) {
-    if(req.query.user === 'admin' && req.query.pass === 'admin') {
+    if(tieneCredenciales(req, 'admin', 'admin')) {
         fs.readFile('./tareas.json', function(e, data){
             var tasks = JSON.parse(data);
             delete tasks[req.params.todoId]
