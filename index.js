e2524c9b2afa0dc28e9b57312c36f83280a4f873
Luis Fernando Mansilla <luis.mansilla@mercadolibre.com>
2018-11-20 21:31:31 -0300

    Middleware de error

--- index.js
+++ index.js
@@ -2,35 +2,28 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+function logDeError(err, req, res, next) {
+    console.log("===ERROR===");
+    console.log(err);
+    console.log("===========");
+    next(err);
+};
+
+function controlDeErrores(err, req, res, next) {
+    res.status(404).send(err.message);
+}
 app.use(bodyParser());
 
-<<<<<<< HEAD
 
-var permisos = {
-    'admin': ['*'],
-    'user': ['POST', 'GET'],
-}
-var checkearPermisosUser = function(req, res, next) {
-    if (req.query.user === 'user' && req.query.pass === 'user') {
+app.use(function(req, res, next) {
+    setTimeout(function(){
+        console.log("Llego una petición!!!");
+        res.locals.name = "luis";
+        res.locals.edad = 80;
         next();
-    } else {
-        res.status(401).send('Sin permisos GATOOOOO 🐈');
-    }
-}
-
-var checkearPermisosAdmin = function(req, res, next) {
-    if (req.query.user === 'admin' && req.query.pass === 'admin') {
-        next();
-    } else {
-        res.status(401).send('Sin permisos GATOOOOO 🐈');
-    }
-}
-
-
-var logger = function(req, res, next) {
-    console.log(req);
-    next();
-}
+    }, 1000);
+});
 
 var obtenerTareas = function(req, res) {
     fs.readFile('./tareas.json', function(e, data){
@@ -38,72 +31,68 @@ var obtenerTareas = function(req, res) {
     });
 }
 
-app.get('/todos', checkearPermisosUser, logger, obtenerTareas);
-
-app.get('/todos/:todoId', checkearPermisosUser, function(req, res) {
-=======
-app.get('/todos', function(req, res) {
-    if (req.query.user === 'admin' && req.query.pass === 'admin' || req.query.user === 'cliente' && req.query.pass === 'cliente'){
-        fs.readFile('./tareas.json', function(e, data){
-            res.send(JSON.parse(data));
-    });
-}
-else {
-    res.status(403).send('No estas autorizado gato 😼');
-}
-});
+app.get('/todos', obtenerTareas);
 
 app.get('/todos/:todoId', function(req, res) {
-    if (req.query.user === 'admin' && req.query.pass === 'admin' || req.query.user === 'cliente' && req.query.pass === 'cliente'){
->>>>>>> a49a6899a670c45833f0c3a75f5c843abb60ce23
     fs.readFile('./tareas.json', function(e, data){
         var tasks = JSON.parse(data);
         res.send(tasks[req.params.todoId]);
     });
-}
-else{
-    res.status(403).send('No estas autorizado gato 😼');
-}
 });
 
-app.post('/todos/:todoId', function(req, res) {
-    if (req.query.user === 'admin' && req.query.pass === 'admin' || req.query.user === 'cliente' && req.query.pass === 'cliente'){
+
+var leerBaseDeDatos = function(req, res, next) {
     fs.readFile('./tareas.json', function(e, data){
-        var tasks = JSON.parse(data);
-        tasks[req.params.todoId] = req.body;
-        fs.writeFile('./tareas.json', JSON.stringify(tasks), function() {
-            res.send(req.params.todoId + ' guardado correctamente');
-        });
+        try {
+            res.locals.data = data;
+            next();
+        } catch (err) {
+            next(err);
+        }
+    });
+};
+
+var parsearDatos = function(req, res, next) {
+    res.locals.data = JSON.parse(res.locals.data);
+    next();
+}
+
+var agregarNuevaTarea = function(req, res, next) {
+    if (res.locals.data[req.params.todoId]) {
+        next(new Error("Ya existe"));
+    } else {
+        res.locals.data[req.params.todoId] = req.body;
+        next();
+    }
+};
+
+var guardarDatos = function(req, res, next) {
+    fs.writeFile('./tareas.json', JSON.stringify(res.locals.data), function(){
+        res.json({ok: true});
     });
 }
-else{
-    res.status(403).send('No estas autorizado gato 😼');
-}
-});
 
-<<<<<<< HEAD
-app.delete('/todos/:todoId',checkearPermisosAdmin, function(req, res) {
+app.post('/todos/:todoId',
+    leerBaseDeDatos, 
+    parsearDatos,
+    agregarNuevaTarea,
+    guardarDatos
+);
+
+app.delete('/todos/:todoId', function(req, res) {
     fs.readFile('./tareas.json', function(e, data){
         var tasks = JSON.parse(data);
         delete tasks[req.params.todoId]
         fs.writeFile('./tareas.json', JSON.stringify(tasks), function(e) {
             res.send(req.params.todoId + ' borrado correctamente');
-=======
-app.delete('/todos/:todoId', function(req, res) {
-    if (req.query.user === 'admin' && req.query.pass === 'admin'){
-        fs.readFile('./tareas.json', function(e, data){
-            var tasks = JSON.parse(data);
-            delete tasks[req.params.todoId]
-            fs.writeFile('./tareas.json', JSON.stringify(tasks), function(e) {
-                res.send(req.params.todoId + ' borrado correctamente');
-            });
->>>>>>> a49a6899a670c45833f0c3a75f5c843abb60ce23
         });
-    } else{
-        res.status(403).send('No estas autorizado gato 😼');
-    }
+    });
 });
+
+app.use(logDeError);
+app.use(controlDeErrores);
 
 app.listen('8080', function(){
     console.log("Escuchando el puerto 8080")
 });
+
